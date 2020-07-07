6467bdb46fdd99528a483b4786e8cd9a11a436c5
Devedse <2350015+devedse@users.noreply.github.com>
2020-07-07 19:17:57 +0200

    Update state.tf

--- terraform/state.tf
+++ terraform/state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "${var.prefix}-TFState"
-    storage_account_name = "${lower(var.prefix)}nltfstate"
-    container_name       = "${lower(var.prefix)}tfazurecomponents"
+    resource_group_name  = "TFState"
+    storage_account_name = "nltfstate"
+    container_name       = "tfazurecomponents"
   }
 }
